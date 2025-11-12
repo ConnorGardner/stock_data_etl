145b2b1314a7873d41a41bdb3fa4fe21b805d743
ConnorGardner <cgardner01@RE-UK-LAP-73405.localdomain>
2025-11-12 16:15:36 -0500

    .gitignore updates

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-zip -j function.zip lambda_function.py
-aws lambda update-function-code --function-name finnhub-api-function --zip-file fileb://function.zip --region us-east-2
-echo "Deployed!"
-
-# This will zip up my lambda_function.py and send it to aws lambda. Lambda will then run it depending on eventbridge configuration.
--- run_finnhub.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export FINNHUB_API_KEY='d3of16pr01quo6o427e0d3of16pr01quo6o427eg'
-export BUCKET_NAME='finnhub-bucket'
-
-python3 lambda_function.py
-
-# Use to test code locally before deploying in aws.
-# Ideally after making code changes, run this, check everything works (data up in s3), and then run ./deploy.sh to send code up to aws lambda.
